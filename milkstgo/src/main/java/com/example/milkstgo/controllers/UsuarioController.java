0fcf4f480576a04173cfa6e2d052eb02d513c21f
diego oliva <diego.oliva@usach.cl>
2023-04-01 00:29:42 -0300

    Creación de archivos iniciales y dependencias springboot

---
+++ milkstgo/src/main/java/com/example/milkstgo/controllers/UsuarioController.java
@@ -0,0 +1,4 @@
+package com.example.milkstgo.controllers;
+
+public class UsuarioController {
+}
---
+++ milkstgo/src/test/java/com/example/milkstgo/MilkstgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milkstgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkstgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
